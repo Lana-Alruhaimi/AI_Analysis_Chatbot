a7c829fa42e0796301f2f07b6c21a3d93cb4f1d8
Lana <lanaalruhaimi@gmail.com>
2025-09-29 16:09:59 +0300

    load data

--- Src/Data_Chatbot.py
+++ Src/Data_Chatbot.py
@@ -12,3 +12,18 @@ os.environ['TRANSFORMERS_NO_ADVISORY_WARNINGS'] = 'true' # So hugging face doesn
 ## LLM info
 GROQ_MODEL = "llama-3.3-70b-versatile"
 HF_MODEL_NAME = "gpt2" #openai-community/gpt2
+
+
+## Load data
+def load_data(): #loads analyzed DataFrame and terminates the app if the file is missing
+    if not os.path.exist(Data_path):
+        st.error(f"ERROR: file not found: {Data_path}")
+        st.warning ("Make sure you have ran (Sentiment_Analysis.py) first")
+        return None
+    
+    try:
+        df = pd.read_pickle(Data_path)
+        return df
+    except Exception as e:
+        st.error(f"ERROR: {e}")
+        return None
