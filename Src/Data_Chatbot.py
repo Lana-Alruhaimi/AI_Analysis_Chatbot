f1b544d6694941347b6e541c73aa79c9ad9d1f40
Lana <lanaalruhaimi@gmail.com>
2025-09-29 19:59:07 +0300

    fixed typo (path . instead of ..)

--- Src/Data_Chatbot.py
+++ Src/Data_Chatbot.py
@@ -5,7 +5,7 @@ from groq import Groq
 from transformers import pipeline, set_seed
 
 ## File paths/directories
-Input_Dir = os.path.join('.', 'Data')
+Input_Dir = os.path.join('..', 'Data')
 Data_path = os.path.join(Input_Dir, 'analyzed_sales_data.pkl')
 os.environ['TRANSFORMERS_NO_ADVISORY_WARNINGS'] = 'true' # So hugging face doesn't send unneeded msg.s
 
