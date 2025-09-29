79313af79a88a64e1c86c96e9ad3050239a3ea19
Lana <lanaalruhaimi@gmail.com>
2025-09-29 15:49:38 +0300

    added model info

--- Src/Data_Chatbot.py
+++ Src/Data_Chatbot.py
@@ -8,3 +8,7 @@ from transformers import pipeline, set_seed
 Input_Dir = os.path.join('.', 'Data')
 Data_path = os.path.join(Input_Dir, 'analyzed_sales_data.pkl')
 os.environ['TRANSFORMERS_NO_ADVISORY_WARNINGS'] = 'true' # So hugging face doesn't send unneeded msg.s
+
+## LLM info
+GROQ_MODEL = "llama-3.3-70b-versatile"
+HF_MODEL_NAME = "gpt2" #openai-community/gpt2
